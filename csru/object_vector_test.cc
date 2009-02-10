b638ba9df71130f2b52c831f5a7b87531e56588a
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2009-02-10 16:24:17 +0000

    Add test for Conv.
    git-svn-id: http://dev.ikejima.org/src/svn/sru2/trunk@235 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- csru/object_vector_test.cc
+++ csru/object_vector_test.cc
@@ -5,10 +5,28 @@
 #include "testing.h"
 #include "object_vector.h"
 
+#include <cassert>
+#include "basic_object.h"
+#include "object_pool.h"
+
 using namespace sru;
+using namespace sru::allocator;
 using namespace std;
 
 
 TEST(ObjectVector_conv){
+  assert(ObjectPool::Instance()->Size() == 0);
+  ptr_vector v1;
+  v1.push_back(BasicObject::New());
+  v1.push_back(BasicObject::New());
+  v1.push_back(BasicObject::New());
+  assert(ObjectPool::Instance()->Size() == 3);
+  ObjectPool::Instance()->GarbageCollect();
+  assert(ObjectPool::Instance()->Size() == 3);
+  object_vector v2 = Conv(v1);
+  v1.clear();
+  assert(ObjectPool::Instance()->Size() == 3);
+  ObjectPool::Instance()->GarbageCollect();
+  assert(ObjectPool::Instance()->Size() == 0);
 }
 

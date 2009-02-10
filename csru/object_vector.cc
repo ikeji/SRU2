73c4e1f07dea0a787548bb7372e063d123fe9f93
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2009-02-10 16:23:48 +0000

    Add object_vector and ptr_vector.
    git-svn-id: http://dev.ikejima.org/src/svn/sru2/trunk@225 f8368ae4-1e99-dc11-9799-0018f35ab5fa

---
+++ csru/object_vector.cc
@@ -0,0 +1,30 @@
+// Programing Language SRU
+// Copyright(C) 2005-2008 IKeJI
+// 
+
+#include "object_vector.h"
+#include "basic_object.h"
+
+namespace sru {
+
+ptr_vector Conv(const object_vector& src){
+  ptr_vector dst;
+  for(object_vector::const_iterator it = src.begin();
+      it != src.end();
+      it++){
+    dst.push_back(BasicObjectPtr(*it));
+  }
+  return dst;
+}
+
+object_vector Conv(const ptr_vector& src){
+  object_vector dst;
+  for(ptr_vector::const_iterator it = src.begin();
+      it != src.end();
+      it++){
+    dst.push_back((*it).get());
+  }
+  return dst;
+}
+
+}  // namespace sru
---
+++ csru/object_vector.h
@@ -0,0 +1,23 @@
+// Programing Language SRU
+// Copyright(C) 2005-2008 IKeJI
+// 
+
+#ifndef OBJECT_VECTOR_H_
+#define OBJECT_VECTOR_H_
+
+#include <vector>
+
+namespace sru {
+
+class BasicObject; 
+class BasicObjectPtr;
+
+typedef std::vector<BasicObject*> object_vector;
+typedef std::vector<BasicObjectPtr> ptr_vector;
+
+ptr_vector Conv(const object_vector& src);
+object_vector Conv(const ptr_vector& src);
+
+} // namespace sru
+
+#endif  // OBJECT_VECTOR_H_

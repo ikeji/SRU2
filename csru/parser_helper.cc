16915898b30bca07e0ac0d3b0431d98cbed803a7
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2010-02-05 18:02:05 +0000

    Add parser help methods.
    git-svn-id: http://dev.ikejima.org/src/svn/sru2/trunk@502 f8368ae4-1e99-dc11-9799-0018f35ab5fa

---
+++ csru/parser_helper.cc
@@ -0,0 +1,121 @@
+// Programing Language SRU
+// Copyright(C) 2005-2010 IKeJI
+// 
+
+#include <string>
+#include <iostream>
+#include "native_proc.h"
+#include "string.h"
+#include "numeric.h"
+#include "library.h"
+
+// TODO: remove this dependency
+#include "testing_ast.h"
+
+using namespace sru;
+using namespace std;
+using namespace sru_test;
+
+namespace sru_parser {
+
+DEFINE_SRU_PROC(spc){
+  assert(arg.size() > 2);
+  string str = SRUString::GetValue(arg[1]);
+  int pos = SRUNumeric::GetValue(arg[2]);
+  int epos = pos;
+#if DEBUG
+  cout << "spc start: pos = " << epos;
+#endif
+  while(true){
+    if(epos > (int)str.size()) break;
+    if(str[epos] != ' ') break;
+    epos++;
+  }
+#if DEBUG
+  cout << "spc result: epos = " << epos;
+#endif
+  BasicObjectPtr ret = BasicObject::New();
+  ret->Set("status", Library::Instance()->True());
+  ret->Set("pos", SRUNumeric::New(epos));
+  return ret;
+}
+
+DEFINE_SRU_PROC(number){
+  assert(arg.size() > 2);
+  string str = SRUString::GetValue(arg[1]);
+  int pos = SRUNumeric::GetValue(arg[2]);
+  int epos = pos;
+  while(true){
+    if(epos > (int)str.size()) break;
+    if(str[epos] < '0' || '9' < str[epos]) break;
+    epos++;
+  }
+  BasicObjectPtr ret = BasicObject::New();
+  if(pos == epos){
+    ret->Set("status", Library::Instance()->False());
+  }else{
+    string substr = str.substr(pos, epos-pos);
+    ret->Set("status", Library::Instance()->True());
+    ret->Set("ast", C(R(R("Numeric"),"parse"),R("Numeric"),S(substr)));
+    ret->Set("pos", SRUNumeric::New(epos));
+  }
+  return ret;
+}
+
+DEFINE_SRU_PROC(id){
+  cout << "checkid" << endl;
+  assert(arg.size() > 2);
+  string str = SRUString::GetValue(arg[1]);
+  int pos = SRUNumeric::GetValue(arg[2]);
+  int epos = pos;
+  while(true){
+    if(epos > (int)str.size()) break;
+    if(
+        (str[epos] < 'a' || 'z' < str[epos]) &&
+        (str[epos] < 'A' || 'Z' < str[epos]) &&
+        (str[epos] < '0' || '9' < str[epos]) &&
+        (str[epos] != '_') &&
+        true
+        ) break;
+    epos++;
+  }
+  BasicObjectPtr ret = BasicObject::New();
+  if(pos == epos){
+    ret->Set("status", Library::Instance()->False());
+  }else{
+    string substr = str.substr(pos, epos-pos);
+    cout << "Match to id: " << substr << endl;
+    ret->Set("status", Library::Instance()->True());
+    ret->Set("ast", R(substr));
+    ret->Set("pos", SRUNumeric::New(epos));
+  }
+  return ret;
+}
+
+DEFINE_SRU_PROC(stringliteral){
+  assert(arg.size() > 2);
+  string str = SRUString::GetValue(arg[1]);
+  int pos = SRUNumeric::GetValue(arg[2]);
+  BasicObjectPtr ret = BasicObject::New();
+  int epos = pos;
+  if(str[pos] != '"'){  // this is not string literal.
+    ret->Set("status", Library::Instance()->False());
+    return ret;
+  }
+  while(true){
+    if(epos > (int)str.size()) break;
+    if(str[epos] == '"') break;
+    epos++;
+  }
+  if(str[epos] != '"'){ // This is't complete string literal
+    ret->Set("status", Library::Instance()->False());
+  }else{
+    string substr = str.substr(pos+1, epos-pos-1); // cut " s
+    ret->Set("status", Library::Instance()->True());
+    ret->Set("ast", S(substr));
+    ret->Set("pos", SRUNumeric::New(epos));
+  }
+  return ret;
+}
+
+}  // namespace sru_parser

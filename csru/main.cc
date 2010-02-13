43d4b0a85351f8f7edb90241b69401ac596f2171
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2010-02-13 16:30:50 +0000

    Initial commit main.
    git-svn-id: http://dev.ikejima.org/src/svn/sru2/trunk@547 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- csru/main.cc
+++ csru/main.cc
@@ -2,5 +2,23 @@
 // Copyright(C) 2005-2008 IKeJI
 // 
 
+#include <iostream>
+#include <string>
+#include "interpreter.h"
+#include "basic_object.h"
+
+using namespace std;
+using namespace sru;
+
 int main(){
+  cout << endl;
+  cout << "C-SRU" << endl;
+  while(true){
+    cout << ">> ";
+    string source;
+    getline(cin, source);
+    BasicObjectPtr r = Interpreter::Instance()->Eval(source);
+    cout << r->Inspect() << endl;
+    cout << endl;
+  }
 }
